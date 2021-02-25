adb84aeadb506b707217d3e8dc41791141b86438
Sindbadia <52345388+Sindbadia@users.noreply.github.com>
2021-02-25 20:50:03 +0200

    FEAT(auth): Add signup & signin

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,6 +1,46 @@
-import { UseGuards } from '@nestjs/common'
+import { ConflictException, InternalServerErrorException } from '@nestjs/common'
 import { EntityRepository, Repository } from 'typeorm'
-import { User } from './user.entiity'
+import { AuthCredentialsDto } from './dto/auth-credentials.dto'
+import { User } from './user.entity'
+import * as bcrypt from 'bcrypt'
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto
+
+    const user = new User()
+    user.username = username
+    user.salt = await bcrypt.genSalt(10)
+    user.password = await this.hashPassword(password, user.salt)
+
+    try {
+      await user.save()
+    } catch (err) {
+      // duplicate username
+      if (err.code === '23505') {
+        throw new ConflictException('Username already exists')
+      } else {
+        throw new InternalServerErrorException()
+      }
+    }
+  }
+
+  async validateUserPassword(
+    authCredentialsDto: AuthCredentialsDto
+  ): Promise<string> {
+    const { username, password } = authCredentialsDto
+
+    const user = await this.findOne({ username })
+
+    if (user && (await user.validatePassword(password))) {
+      return user.username
+    } else {
+      return null
+    }
+  }
+
+  private async hashPassword(password: string, salt: string): Promise<string> {
+    return bcrypt.hash(password, salt)
+  }
+}
